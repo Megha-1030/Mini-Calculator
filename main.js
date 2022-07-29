7903686754893b4b2e7d944e2809184ea329f7be
Megha-1030 <110226917+Megha-1030@users.noreply.github.com>
2022-07-29 14:17:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function add(){
+    var result=Number(document.getElementById("fn").value)+Number(document.getElementById("sn").value);
+document.getElementById("b1").value=result;
+}
+
+function subtract(){
+    var result=Number(document.getElementById("fn").value)-Number(document.getElementById("sn").value);
+document.getElementById("b2").value=result; 
+}
+
+function multiply(){
+    var result=Number(document.getElementById("fn").value)*Number(document.getElementById("sn").value);
+document.getElementById("b3").value=result; 
+}
+
+function divide(){
+    var result=Number(document.getElementById("fn").value)/Number(document.getElementById("sn").value);
+    document.getElementById("b4").value=result;
+}
